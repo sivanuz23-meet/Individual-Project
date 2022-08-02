8821f5407ccffb44361288ab92b89b480fce3f07
sivanuz23-meet <109904738+sivanuz23-meet@users.noreply.github.com>
2022-08-02 18:16:21 +0300

    Beginning

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -5,12 +5,61 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
+config = {
 
+  "apiKey": "AIzaSyCuS-h0Gw4gFtQJAuaPw15pnN9nEn1ZvxU",
 
+  "authDomain": "individual-project-a2091.firebaseapp.com",
 
+  "databaseURL": "https://individual-project-a2091-default-rtdb.europe-west1.firebasedatabase.app",
 
-#Code goes above here
+  "projectId": "individual-project-a2091",
+
+  "storageBucket": "individual-project-a2091.appspot.com",
+
+  "messagingSenderId": "422798657909",
+
+  "appId": "1:422798657909:web:fc4cac3e10d4e59a7a2b3c",
+
+  "measurementId": "G-R71QNN6H3L"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = database.firebase()
+
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			return redirect(url_for('home'))
+		except:
+			error = "Authentication failed"
+			print (error)
+	return render_template("signup.html")
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.signin_with_user_email_and_password(email, password)
+			return redirect(url_for('home'))
+		except:
+			error = "Authentication failed"
+			print (error)
+	return render_template("signin.html")
+
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+	return render_template('home.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
